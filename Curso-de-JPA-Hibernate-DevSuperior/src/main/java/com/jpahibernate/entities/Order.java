4982c5108a06eed98680c67a504d3a90311b8521
Carlos Eduardo <eduardolcmdeveloper@gmail.com>
2024-01-15 17:53:47 -0300

    Criacao de enum OrderStatus

--- Curso-de-JPA-Hibernate-DevSuperior/src/main/java/com/jpahibernate/entities/Order.java
+++ Curso-de-JPA-Hibernate-DevSuperior/src/main/java/com/jpahibernate/entities/Order.java
@@ -1,6 +1,7 @@
 package com.jpahibernate.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jpahibernate.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -23,17 +24,19 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
-    private Order(){
+    private Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -51,6 +54,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
