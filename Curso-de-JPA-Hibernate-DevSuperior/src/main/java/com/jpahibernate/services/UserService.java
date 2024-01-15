265eca9c5d0fdecf85597f1eb8f1a3c0f8a872b6
Carlos Eduardo <eduardolcmdeveloper@gmail.com>
2024-01-15 15:44:46 -0300

    Criacao do findById

---
+++ Curso-de-JPA-Hibernate-DevSuperior/src/main/java/com/jpahibernate/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jpahibernate.services;
+
+import com.jpahibernate.entities.User;
+import com.jpahibernate.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+
+}
